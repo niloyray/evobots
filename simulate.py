187e32306a4acbdd3e2336babe095b9759bf3fb7
niloyray <rayniloy@gmail.com>
2021-07-07 22:04:57 +0530

    Modified simulate.py

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time
+from datetime import datetime
 
 physicsClient = p.connect(p.GUI)
+
+startTime = datetime.now() 
+for i in range(500):
+  p.stepSimulation()
+  time.sleep(0.01)
+print("  >>> Time Taken: ",datetime.now() - startTime) 
+
 p.disconnect()
