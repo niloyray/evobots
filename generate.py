a2c38165070a82c7d934d4848a32efd724290d57
niloyray <rayniloy@gmail.com>
2022-05-11 23:16:16 +0530

    Added simulate.py and generate.py

---
+++ generate.py
@@ -0,0 +1,26 @@
+import pyrosim.pyrosim as pyrosim
+
+
+x = 0;
+
+pyrosim.Start_SDF("boxes.sdf");
+
+for i in range(10):
+  x+=1
+  y = 0
+  for j in range(10):
+    y += 1
+    a = 1
+    z = 0.5
+    length = 1
+    width = 1
+    height = 1
+    for k in range(10):
+      pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height]);
+      length = length*0.9;
+      width = width*0.9;
+      height = height*0.9;
+      a += height;
+      z=a-height/2;
+
+pyrosim.End();
--- simulate.py
+++ simulate.py
@@ -1,13 +1,21 @@
+import pybullet_data
 import pybullet as p
 import time
 from datetime import datetime
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath());
+
+p.setGravity(0, 0, -9.8);
+planeId = p.loadURDF("plane.urdf");
+
+p.loadSDF("boxes.sdf");
 
 startTime = datetime.now() 
-for i in range(500):
+for i in range(1000):
   p.stepSimulation()
   time.sleep(0.01)
 print("  >>> Time Taken: ",datetime.now() - startTime) 
+
 
 p.disconnect()
