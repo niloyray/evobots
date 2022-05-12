51c93018283303717f1ca7c7aea5870f3ece1f0f
niloyray <rayniloy@gmail.com>
2022-05-12 21:13:40 +0530

    pyrosim.py line 99 jointInfo[1] >> jointInfo[1].decode('UTF-8')

--- simulate.py
+++ simulate.py
@@ -23,8 +23,11 @@ pyrosim.Prepare_To_Simulate(robotId);
 startTime = datetime.now() 
 for i in range(2000):
   p.stepSimulation()
+  
   backLegTouchSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg");
   frontLegTouchSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg");
+  pyrosim.Set_Motor_For_Joint( bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500);
+  
   time.sleep(0.01)
 print("  >>> Time Taken: ",datetime.now() - startTime) 
 
